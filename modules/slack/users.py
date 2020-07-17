1aec13104d36e15db765736087574db79cfb20e9
yokonao <yokotukanao@me.com>
2020-07-17 20:30:51 +0900

    refactor: Use environmental variable in users dictionary

--- modules/slack/users.py
+++ modules/slack/users.py
@@ -1,1 +1,8 @@
-users = {'U014WTGDGFJ': 'Arakaki', 'U0140AQV9L6': 'Taishow', 'U014EL0EZ2Q': 'Yokotsuka'}
+import os
+
+# users = {'U014WTGDGFJ': 'Arakaki', 'U0140AQV9L6': 'Taishow', 'U014EL0EZ2Q': 'Yokotsuka'}
+users = {}
+
+users[os.getenv('TCI_USER1_ID')] = os.getenv('TCI_USER1_NAME')
+users[os.getenv('TCI_USER2_ID')] = os.getenv('TCI_USER2_NAME')
+users[os.getenv('TCI_USER3_ID')] = os.getenv('TCI_USER3_NAME')
